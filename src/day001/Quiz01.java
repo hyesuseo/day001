0d243a871cf42830edc8aaa27707a53ae27f7686
hyet <hyesuc_seo@naver.com>
2024-09-26 22:30:57 +0900

    퀴즈오답수정

--- src/day001/Quiz01.java
+++ src/day001/Quiz01.java
@@ -6,9 +6,11 @@ public class Quiz01 {
 		System.out.println("이름\t나이\t전화번호\t\t회비\t");
 		System.out.println("======================================");
 		System.out.print("홍길동\t\"");
-		System.out.println("15\"\t"+1234+"-"+3456);
-		System.out.println("고길동\t\"15\"\t2238-1234\t\\30000\t");
-		System.out.println("김말이\t\"15\"\t1234-1234\t\\50000\t");
+		System.out.println("15\"\t"+1234+"-"+3456+"\t"+"\\20000");
+		System.out.print("고길동\t\"");
+		System.out.println("15\"\t"+2238+"-"+1234+"\t"+"\\30000\t");
+		System.out.print("김말이\t\"");
+		System.out.println("15\"\t"+1234+"-"+1234+"\t"+"\\50000\t");
 		System.out.println("--------------------------------------");
 		System.out.println("총합계\t\t\t\t\\100000\t");
 		System.out.println("--------------------------------------");
